fa6e1ec878da4aa5d0c8f2a025149ec095b701b4
asafravid <asaf.rvd@gmail.com>
2021-09-04 21:34:51 +0300

    - Known weights and Unknown weights

--- main.py
+++ main.py
@@ -132,20 +132,24 @@ def post_process_etfs(csv_db_path, csv_db_filename):
                 if len(row) < len(title_row):
                     row_index += 1
                     continue
-                if row[2] == '' or
-                sum_weights = float(row[3])+float(row[5])+float(row[7])+float(row[9])+float(row[11])+float(row[13])+float(row[15])+float(row[17])+float(row[19])+float(row[21])
-                # TODO: ASAFR: Sum the known Symbols weights (non '') and the unknown(=='') symbol weights
-                #if sum_weights > 2:
-                #    row_index += 1
-                #    continue
-                row.append(sum_weights)
+
+                sum_weights_known = 0
+                sum_weights_unknown = 0
+                for weight_index in range(3, 21+1, 2):
+                    if row[weight_index-1] == '':
+                        sum_weights_unknown += float(row[weight_index])
+                    else:
+                        sum_weights_known   += float(row[weight_index])
+
+                row.append(sum_weights_known)
+                row.append(sum_weights_unknown)
                 filtered_db_rows_data.append(row)
                 row_index += 1
 
-    title_row.append('SumWeights')
+    title_row.append('SumWeightsKnown')
+    title_row.append('SumWeightsUnknown')
     filtered_db_rows_data.insert(0, title_row)
     filtered_csv_db_filename = csv_db_path+'filtered_weighted_'+csv_db_filename
-
 
     with open(filtered_csv_db_filename, mode='w', newline='') as engine:
         writer = csv.writer(engine)
