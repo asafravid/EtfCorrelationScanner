ee16b45f17a60a5ab8b86cecf01c83b3500a57cf
asafravid <asaf.rvd@gmail.com>
2021-09-08 23:12:16 +0300

    - PDF unified results to 1 file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #############################################################################
 #
-# Version 0.0.20 - Author: Asaf Ravid <asaf.rvd@gmail.com>
+# Version 0.0.21 - Author: Asaf Ravid <asaf.rvd@gmail.com>
 #
 #    ETF Correlation  Scanner - based on yfinance
 #    Copyright (C) 2021 Asaf Ravid
