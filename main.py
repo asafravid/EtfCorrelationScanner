feea2d9e6afe5edd752d49a112f78a52901511e5
Asaf Ravid <asaf.rvd@gmail.com>
2021-10-30 20:01:02 +0300

    - Add 20211030 Results

--- main.py
+++ main.py
@@ -43,8 +43,8 @@ from contextlib import closing
 # Start of Run Configuration ###########
 SCAN_ETFS                   = False
 POST_PROCESS_ETFS           = True
-POST_PROCESS_PATH_NEW       = '20211016-064338'
-POST_PROCESS_PATH_REF       = '20211002-044815'
+POST_PROCESS_PATH_NEW       = '20211030-161741'
+POST_PROCESS_PATH_REF       = '20211030-161741'
 CUSTOM_ETF_LIST             = None  # ['QQQ', 'SPY', 'FDIS', 'SMH', 'SOXX']
 NUM_REPORTED_ENTRIES        = 42
 NUM_REPORTED_BIGRAM_ENTRIES = 77
