80981f351ae19c3d783e2ee8a68fcad44d80b984
Asaf Ravid <aravid@marvell.com>
2022-01-08 22:09:39 +0200

    - Add 20220108 Results

--- main.py
+++ main.py
@@ -42,10 +42,10 @@ from contextlib import closing
 #              4. Some tabels don't have +New and +Removed when crossing the report boundary - fix
 
 # Start of Run Configuration ###########
-SCAN_ETFS                   = False
-POST_PROCESS_ETFS           = True
-POST_PROCESS_PATH_NEW       = '20211225-013423'
-POST_PROCESS_PATH_REF       = '20211212-032759'
+SCAN_ETFS                   = True
+POST_PROCESS_ETFS           = False
+POST_PROCESS_PATH_NEW       = '20220108-215153'
+POST_PROCESS_PATH_REF       = '20220108-215153'
 CUSTOM_ETF_LIST             = None  # ['QQQ', 'SPY', 'FDIS', 'SMH', 'SOXX']
 NUM_REPORTED_ENTRIES        = 42
 NUM_REPORTED_BIGRAM_ENTRIES = 77
